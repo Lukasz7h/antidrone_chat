0afd5019f5f909ceace249a4c81d6ac00f646e55
itelite <lukasz.trzepadlek@itelite.net>
2023-10-25 15:41:46 +0200

    upd
    Chat app created in electron library and angular framework. That app goal is made communication way between guests and company members.

--- chat/src/app/app.component.ts
+++ chat/src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { Socket } from 'ngx-socket-io';
+import { webSocket } from 'rxjs/webSocket';
 
 @Component({
   selector: 'app-root',
@@ -8,16 +8,22 @@ import { Socket } from 'ngx-socket-io';
 })
 export class AppComponent implements OnInit{
   title = 'chat';
+  private socketSubject;
 
-  constructor(private socket: Socket){}
-
+  constructor(){
+    const secret_key = "cz9Dsdsiyas986adsfOHzz4dzzAPOwdMQNzzzmlocbcv869fiasTYdawn648489z49S8DadwajuhkjlA5S3A4adefdsaUfaa5AF654dasSA64a56fs46a5f46adfs657dfs897dsfd89ds9d87sa3dAFw4a2dascxzASFzzASFaaO76G96fafa98sad";
+    this.socketSubject = webSocket("ws://localhost:3000/?key="+secret_key);
+  }
+   
   ngOnInit(): void {
-    this.socket.connect();
 
-    this.socket.on("message", (data: any) => {
+    this.socketSubject.subscribe((data) => {
       console.log(data);
     });
 
-    this.socket.emit("message", {data: true})
+    setTimeout(() => {
+      this.socketSubject.next({message: {data: "know"}})
+    }, 500);
+    
   }
 }
